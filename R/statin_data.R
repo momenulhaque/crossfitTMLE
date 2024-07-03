dabaff5df809f88acf9a2e3c99e50abec374b9fe
Momenul Haque Mondol <mmondol@isrt.ac.bd>
2024-07-03 16:58:28 -0700

    updated data

--- R/statin_data.R
+++
@@ -1,8 +0,0 @@
-#' Description of statin_sim_data
-#'
-#' This dataset contains information about 3000 patients (simulated) on outcome (Y), statin use (statin), age, Low-density lipoprotein (ldl_log),
-#' diabetes, risk score, risk score categories. The simulation details are described in Zivich and Breskin (2021).
-#'
-#' @format A data frame with 3000 rows and 7 columns.
-"statin_sim_data"
-
